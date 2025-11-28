4c0c15c7bf3485baab81f4a91186c169fb33eedf
jscottym <scott@expandedlife.com>
2025-11-28 12:54:16 -0700

    chore: update Nuxt configuration to use main.css and integrate Tailwind CSS with Vite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@pinia/nuxt', '@vueuse/nuxt'],
-  css: ['~/assets/css/tailwind.css']
+  css: ['./app/assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
